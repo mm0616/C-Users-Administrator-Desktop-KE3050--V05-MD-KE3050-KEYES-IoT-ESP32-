d7c3f2c3cf92e0d541e146cd482711d49e1b5b35
momo <1668817352@qq.com>
2025-03-20 08:32:40 +0800

    first commit

---
+++ docs/项目代码+驱动文件+库文件/项目代码/Python 代码/Project_06.1_servo.py
@@ -0,0 +1,26 @@
+# 作者 : www.keyes-robot.com
+
+from machine import Pin, PWM
+import time
+pwm = PWM(Pin(13))  
+pwm.freq(50)
+
+'''
+与角度对应的占空比
+0°----2.5%----25
+45°----5%----51.2
+90°----7.5%----77
+135°----10%----102.4
+180°----12.5%----128
+'''
+angle_0 = 25
+angle_90 = 77
+angle_180 = 128
+
+while True:
+    pwm.duty(angle_0)
+    time.sleep(1)
+    pwm.duty(angle_90)
+    time.sleep(1)
+    pwm.duty(angle_180)
+    time.sleep(1)
